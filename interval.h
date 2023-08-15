20a9c2858349a1146a82dc8e6c66bc11ac867bb9
Ivan Shuvalov <vanyashuvalov@gmail.com>
2023-08-15 18:15:52 -0400

    initial commit

---
+++ interval.h
@@ -0,0 +1,41 @@
+#pragma once
+
+class interval {
+  public:
+    double min, max;
+
+    interval() : min(+infinity), max(-infinity) {} // Default interval is empty
+
+    interval(double _min, double _max) : min(_min), max(_max) {}
+
+    interval(const interval& a, const interval& b)
+      : min(fmin(a.min, b.min)), max(fmax(a.max, b.max)) {}
+
+    double clamp(double x) const {
+        if (x < min) return min;
+        if (x > max) return max;
+        return x;
+    }
+
+    double size() const {
+        return max - min;
+    }
+
+    interval expand(double delta) const {
+        auto padding = delta/2;
+        return interval(min - padding, max + padding);
+    }
+
+    bool contains(double x) const {
+        return min <= x && x <= max;
+    }
+
+    bool surrounds(double x) const {
+        return min < x && x < max;
+    }
+
+    static const interval empty, universe;
+};
+
+const static interval empty(+infinity, -infinity);
+const static interval universe(-infinity, +infinity);
